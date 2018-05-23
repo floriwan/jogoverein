a52d690467c95b63a56ae6833121eb5da6e9a1ec
Florian <florian@goeldenitz.org>
2018-05-23 18:52:17 +0200

    add dsgvo page

---
+++ datenschutz.php
@@ -0,0 +1,79 @@
+<!DOCTYPE HTML>
+<!--
+	Alpha by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>JoGoVEREIN - Datenschutz</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body>
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<?php include ("header.php"); ?>
+
+			<!-- Main -->
+				<section id="main" class="container">
+					<header>
+						<h2>Datenschutz</h2>
+					</header>
+					<div class="box">
+					
+                    <p>Hier informieren ich Sie als Besucher meiner Website gemäß den einschlägigen Richtlinien 
+                    (europäische Datenschutz-Grundverordnung „DSGVO“, Bundesdatenschutzgesetz „BDSG“) über die Art, 
+                    den Umfang und die Zwecke der Erhebung und Verwendung personenbezogener Daten, die durch Ihren Besuch entstehen.</p>
+                    
+                    <p><bold>Ich erhebe keinerlei Daten aus der Nutzung meines Webauftritts und beziehe auch keine Daten über Ihre Nutzung 
+                    meines Webangebots von meinem Web-Dienstleister, der meine Website im Internet bereitstellt („Hoster“).</p>
+                    
+                    <p>Wenn Sie weitergehende Fragen zum Datenschutz im Zusammenhang mit meinem Internetauftritt haben, 
+                    wenden Sie sich bitte an mich (siehe Impressum).</p>
+                    
+                    <h3>Information über meinen Web-Dienstleister</h3>
+
+                    <p>Bei jedem Aufruf dieser Website übermittelt Ihr Internetbrowser Daten an den Betreiber des Webservers. 
+                    Aus technischen Gründen ist dies notwendig. Unser Webauftritt wird von Webservern des Unternehmens „Host Europe“ 
+                    im Internet bereitgestellt („gehostet“). (Host Europe GmbH, Hansestrasse 111, 51149 Köln, 
+                    <a href="https://www.hosteurope.de/">https://www.hosteurope.de/</a>).
+
+                    <p>Üblicherweise werden folgende Daten beim Aufruf einer Website aufgezeichnet:</p>
+
+                    <ul>
+                    <li>Datum und Uhrzeit der Anforderung
+                    <li>Name der angeforderten Datei
+                    <li>Seite, von der aus die Datei angefordert wurde
+                    <li>Zugriffsstatus (Datei übertragen, Datei nicht gefunden, etc.)
+                    <li>verwendeter Webbrowser und verwendetes Betriebssystem
+                    <li>Bildschirmgröße und -auflösung
+                    <li>vollständige IP-Adresse des anfordernden Rechners
+                    <li>übertragene Datenmenge
+                    </ul>
+
+                    <p>Nähere Einzelheiten zur Datenerhebung und -speicherung von Host Europe sehen Sie unter: 
+                    <a href="https://www.hosteurope.de/AGB/Datenschutzerklaerung/">https://www.hosteurope.de/AGB/Datenschutzerklaerung/</a>.</p>
+                    
+				</section>
+
+			<!-- Footer -->
+				<?php include ("footer.html"); ?>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrollgress.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
--- index.php
+++ index.php
@@ -43,13 +43,8 @@
             </section>
         
             <!-- Footer -->
-            <footer id="footer">
-                <ul class="copyright">
-                    <li>&copy; Untitled. All rights reserved.</li>
-                    <li><a href="impressum.php">Impressum</a></li>
-                    <li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
-                </ul>
-            </footer>
+            <?php include ("footer.html"); ?>
+            
         </div>
     
         <!-- Scripts -->
